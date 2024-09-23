2f219be692a31006e1f212ad27154803eae9a550
Fabian Darga <kontakt@fabiandarga.de>
2024-09-23 05:33:32 +0200

    Setup

---
+++ src/main/java/de/fabiandarga/Main.java
@@ -0,0 +1,16 @@
+package de.fabiandarga;
+
+public class Main {
+    public static void main(String[] args) {
+        // Let's start with a very simple version of a turn based text adventure
+        System.out.println("Welcome to the Text Adventure!");
+
+        // We need an input scanner
+
+        // Then we start a loop
+        // Read from  input
+        // execute some command
+        // quit the loop when "quit" was entered or then the game is over
+
+    }
+}
