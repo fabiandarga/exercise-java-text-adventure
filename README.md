9bbc67424d8d1995d11ebc04eaadfd1958c56f14
Fabian Darga <kontakt@fabiandarga.de>
2024-09-23 15:49:17 +0200

    Create basic input loop

---
+++ README.md
@@ -0,0 +1,29 @@
+# Guided Exercise: Terminal Game
+
+## Day 1
+Let's start with a very simple version of a turn based text adventure.
+
+### Game Loop & Input
+First we add a game loop which constantly prompts the player for input and then executes some game action.
+
+### Game State
+Let's talk about State Machine.
+Then we add a way to handle "State" 
+in our game.
+
+### Commands
+Commands describe the change to our game state.  
+Let us start with the move command.
+
+
+## Day 2
+We will add some game idea, a win and a loss condition.
+Add more commands to collect items and show collected items.
+Also let's save the current game state to our database.
+
+
+## Day 3
+We will create random items when a new game is started.
+Let's add caching to our app.
+
+## Day 4
--- src/main/java/de/fabiandarga/Main.java
+++ src/main/java/de/fabiandarga/Main.java
@@ -1,16 +1,38 @@
 package de.fabiandarga;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Let's start with a very simple version of a turn based text adventure
         System.out.println("Welcome to the Text Adventure!");
+        System.out.println("-----------------------------");
+
+        System.out.println("Enter \"quit\" to quit");
+
 
         // We need an input scanner
+        Scanner scanner = new Scanner(System.in);
+
+        boolean gameRunning = true;
 
         // Then we start a loop
-        // Read from  input
-        // execute some command
-        // quit the loop when "quit" was entered or then the game is over
+        while (gameRunning) {
+            System.out.print("> ");
+            // Read from  input
+            String input = scanner.nextLine();
 
+            // execute some command
+            switch (input) {
+                case "quit":
+                    // quit the loop when "quit" was entered or then the game is over
+                    gameRunning = false;
+                    break;
+                case null, default:
+                    System.out.println("Wrong input!");
+            }
+        }
+
+        System.out.println("Goodbye, see you next time!");
     }
 }
